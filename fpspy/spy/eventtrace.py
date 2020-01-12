87b6f5216965c4878d81c6b31fb64ae719aa4fb2
Alex Bernat <abernat@bernat.io>
2020-01-12 14:20:40 -0500

    csv output issues

--- fpspy/spy/eventtrace.py
+++ fpspy/spy/eventtrace.py
@@ -5,19 +5,20 @@ if(len(sys.argv)!=2):
     sys.exit("Error - needs time interval in ms")
 interval = int(sys.argv[1])
 events = 0
+first_line = sys.stdin.readline()
+first_line=first_line.split()
+initialmillis, prevmillis = int(first_line[0])
 with open('trace.csv', 'w+') as csvfile:
     csvwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
     csvwriter.writerow(['Time,','Events'])
     for line in sys.stdin:
         results = line.split()
         millis = int(results[0])
-        initialmillis = millis
-        prevmillis = millis
-        events = events +1
-        csvwriter.writerow([str(prevmillis)+",",str(events)])
-        '''
-        if(millis-prevmillis>=interval):
-            csvwriter.writerow([str(prevmillis)+",",str(events)])
+        
+        events = events + 1
+
+        if(millis-prevmillis > interval)
+            csvwriter.writerow([str(prevmillis-initialmillis)+",",str(events)])
             prevmillis=millis
             events=0
-            '''
+        
