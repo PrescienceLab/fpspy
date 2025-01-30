0057b31a0e11d1482556a888a9a2d7c6ae6ee73a
Liam Strand <strandliam@gmail.com>
2025-01-30 16:52:19 -0600

    make seperate test cases for sim/synth runs

---
+++ test/no_nanny.c
@@ -0,0 +1,37 @@
+/*
+
+  Part of FPSpy
+
+  Test code for floating point exception interception
+
+  Copyright (c) 2017 Peter A. Dinda - see LICENSE
+
+*/
+
+#include <util.h>
+#define __GNU_SOURCE
+
+void use(double x);
+
+
+void nanny()
+{
+  volatile double x,y,z;
+  
+  x=0.0;
+  y=0.0;
+  z = x/y;
+  use(z);
+}
+
+void use(double x) {}
+
+int main(int argc, char *argv[], char *envp[])
+{
+  write_csr(0x880, 0x1f);
+
+  return 0;
+}
+  
+
+
