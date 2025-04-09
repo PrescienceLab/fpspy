bcbdcfddb31f843721089d6f75da1ce197b10ff6
Peter Dinda <pdinda@northwestern.edu>
2025-04-09 11:46:58 -0500

    add config option to totally disable debug output at compile time; deselect riscv-specific features on non-riscv archs

--- include/debug.h
+++ include/debug.h
@@ -2,6 +2,6 @@
 
 extern unsigned char log_level;
 
-#define DEBUG(S, ...) do { if (log_level > 1) {fprintf(stderr, "fpspy: debug(%8d): " S, gettid(), ##__VA_ARGS__);} } while (0)
+#define DEBUG(S, ...) do { if (!CONFIG_NO_DEBUG_OUTPUT && log_level > 1) {fprintf(stderr, "fpspy: debug(%8d): " S, gettid(), ##__VA_ARGS__);} } while (0)
 #define INFO(S, ...) fprintf(stderr,  "fpspy: info(%8d): " S, gettid(), ##__VA_ARGS__)
 #define ERROR(S, ...) fprintf(stderr, "fpspy: ERROR(%8d): " S, gettid(), ##__VA_ARGS__)
