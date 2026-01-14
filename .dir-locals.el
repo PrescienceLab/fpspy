3e5ccf9c238bb79c853c23308b72014cc509814b
Karl Hallsby <karl@hallsby.com>
2026-01-13 18:45:24 -0600

    Teach Emacs to respect the C indentation we use

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,4 @@
 ((nil . ((eval . (add-hook 'before-save-hook #'delete-trailing-whitespace))))
- (asm-mode . ((comment-start . "//"))))
+ (asm-mode . ((comment-start . "//")))
+ (c-or-c++-mode . ((c-basic-offset . 2)))
+ (c-or-c++-ts-mode . ((c-basic-offset . 2))))
